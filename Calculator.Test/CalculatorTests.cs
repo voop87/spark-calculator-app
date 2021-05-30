8cec807d7233d103b1edbc560bdb4b1c38fbbc2c
Ruslan Mansurov <mansurov87@gmail.com>
2021-05-30 12:05:15 +0700

    Add unit tests

--- Calculator.Test/CalculatorTests.cs
+++ Calculator.Test/CalculatorTests.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Calculator.Test
 {
-    public class CalculatorTests
+    public partial class CalculatorTests
     {
         [Fact]
         public void ShouldDoOperations()
@@ -18,13 +19,51 @@ namespace Calculator.Test
         }
 
         [Fact]
-        public void ShouldBreakWhenDivideByZero()
+        public void ShouldReturnNanWhenDivideByZero()
         {
             double num1 = 4;
             double num2 = 0;
 
             Assert.Equal(double.NaN, MyCalculator.DoOperation(num1, num2, "d"));
+            Assert.Equal(double.NaN, MyCalculator.DoOperation(num1, num2, "w"));
         }
 
+        [Fact]
+        public void ShouldReceiveUserInput()
+        {
+            IConsoleWrapper console = new MockConsoleWrapper();
+            string userInput1 = console.ReadLine();
+            Assert.Equal("5", userInput1);
+            string userInput2 = console.ReadLine();
+            Assert.Equal("2", userInput2);
+            string userInput3 = console.ReadLine();
+            Assert.Equal("m", userInput3);
+            string userInput4 = console.ReadLine();
+            Assert.Equal("n", userInput4);
+        }
+
+        [Fact]
+        public void ShouldMultiply5and2()
+        {
+            MockConsoleWrapper mockConsoleWrapper = new MockConsoleWrapper();
+            MyCalculator.RunCalculator(mockConsoleWrapper);
+        }
+
+        public class MockConsoleWrapper : IConsoleWrapper
+        {
+            Queue<string> UserInputs = new Queue<string>();
+
+
+            public string ReadLine()
+            {
+                UserInputs.Enqueue("5");
+                UserInputs.Enqueue("2");
+                UserInputs.Enqueue("m");
+                UserInputs.Enqueue("n");
+
+                return UserInputs.Dequeue();
+            }
+            
+        }
     }
 }
---
+++ Calculator/ConsoleWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+
+    public interface IConsoleWrapper
+    {
+        string ReadLine();
+    }
+
+    public class ConsoleWrapper : IConsoleWrapper
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+
+}
